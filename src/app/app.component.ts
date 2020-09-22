164bdfe3dd1e5e1cce9e433d1164262c0f5198c7
Reynaldo Quintero <reynaldoquinterom@gmail.com>
2020-09-22 14:58:15 +0200

    Color revamp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component, HostBinding } from '@angular/core';
 })
 export class AppComponent {
   isDark: boolean = true;
-  isUser: boolean =  false;
+  isUser: boolean =  true;
 
   @HostBinding('class')
   get themeMode() {
