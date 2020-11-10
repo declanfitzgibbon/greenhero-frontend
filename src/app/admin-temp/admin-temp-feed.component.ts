ee68b86ec64b62a648d037e9789e9ff94c24cfa5
declanfitzgibbon <55151075+declanfitzgibbon@users.noreply.github.com>
2020-11-10 23:16:47 +0100

    Changes to temp design

--- src/app/admin-temp/admin-temp-feed.component.ts
+++ src/app/admin-temp/admin-temp-feed.component.ts
@@ -1,79 +1,29 @@
 import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { ThemeService } from '../theme.service';
+
 @Component({
   selector: 'app-admin-temp',
   templateUrl: './admin-temp-feed.component.html',
   styleUrls: ['./admin-temp-feed.component.css'],
 })
+
 export class AdminTempFeedComponent implements OnInit {
-
-  isDark: boolean;
-
-  @Input() timeFrame: number = 0;
-
- colorScheme: {domain: Array<string> };
-  single: Array<{name: string, value: number | string}>;
-  view: Array<number> = [innerWidth - 20, innerWidth < 500 ? 500 : (innerHeight / 4) - 40];
-  loading: boolean;
-
+    currentTemp: Array<{
+      title: string;
+      value: number;
+      state: string;
+    }>;
+  
   constructor(private themeService: ThemeService) {
   }
 
   ngOnInit(): void {
-    
-    this.themeService.getThemeType().subscribe((theme) => {
-      this.isDark = theme;
-      if(this.isDark) {
-        this.colorScheme = { domain: ["#375c66", "#37665b"] }
-      } else {
-        this.colorScheme = { domain: ["#68b1c4", "#70ccb7"] }
+    this.currentTemp = [
+      {
+      title: 'Current Temperature',
+      value: 22,
+      state: 'A/C is on'
       }
-    });
-
-    this.isDark = this.themeService.getCurrentThemeType();
-
-    if(this.isDark) {
-      this.colorScheme = { domain: ["#375c66", "#37665b"] }
-    } else {
-      this.colorScheme = { domain: ["#68b1c4", "#70ccb7"] }
-    }
-
-    switch (this.timeFrame) {
-      case 0:
-        this.single = [
-          {
-            name: 'Temp C°',
-            value: 25
-          }
-        ];
-        break;
-
-      case 1:
-          this.single = [ 
-            {
-              name : 'Temp C°',
-              value: 26
-            }
-          ];
-          break;
-
-        default:
-          break;
-      }
-
-      this.loading = false;
-  }
-  
-  ngOnChanges(changes: SimpleChanges) {
-    this.ngOnInit();
-  }
-
-  onSelect(card: {name: string, value: number}) {
-    console.log(card);
-  }
-
-  onResize(event) {
-    this.view = [innerWidth - 20, innerWidth < 500 ? 500 : (innerHeight / 4) - 40];
+    ]
   }
 }
-
