c4c33cac241d732b06a9403a30f1390ec2b4ccba
ethanyang <chuunibyohan@gmail.com>
2020-11-11 21:23:18 +0100

    update

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -1,181 +1,107 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class EventService {
-    events: any;
-    eventEmitter: EventEmitter<any> = new EventEmitter();
+  events: any;
+  eventEmitter: EventEmitter<any> = new EventEmitter();
 
-    constructor() {
-        this.events = [{
-          _id: "1",
-          description: "Lorem Ipsum",
-          name: "Energy Saver",
-          src: "/assets/event-image-1.png",
-          date: new Date(),
-          boss: {
-            _id: "1",
-            name: "Energy Lich",
-            avatar: '/assets/enemy-example.png',
-            description: "LOREM IPSUM!!!!!!",
-            defense: 200,
-            attack: 130,
-            healing_factor: 20,
-            health: 2000
+  constructor(private http: HttpClient) {
+  }
+
+  async getEvents() {
+    return this.http.get<any>('http://localhost:8080/Event/').toPromise();
+  }
+
+  async getMostGrouped(timeFrame: number) {
+    return [
+      {
+        name: 'Developers',
+        value: (await this.http.get<{ total: number }>('http://localhost:8080/Team/getNumberOfPeople', {
+          params: {
+            profession: 'Developers'
           }
-        },
-        {
-          _id: "2",
-          description: "Lorem Ipsum",
-          name: "Water Saver",
-          src: "/assets/event-image-2.png",
-          date: new Date(),
-          boss: {
-            _id: "2",
-            name: "Water Lich",
-            avatar: '/assets/enemy-example.png',
-            description: "LOREM IPSUM!!!!!!",
-            defense: 200,
-            attack: 130,
-            healing_factor: 20,
-            health: 2000
+        }).toPromise()).total
+      },
+      {
+        name: 'Lawyers',
+        value: (await this.http.get<{ total: number }>('http://localhost:8080/Team/getNumberOfPeople', {
+          params: {
+            profession: 'Lawyers'
           }
-        }];
-    }
+        }).toPromise()).total
+      },
+      {
+        name: 'Designers',
+        value: (await this.http.get<{ total: number }>('http://localhost:8080/Team/getNumberOfPeople', {
+          params: {
+            profession: 'Designers'
+          }
+        }).toPromise()).total
+      },
+      {
+        name: 'Engineers',
+        value: (await this.http.get<{ total: number }>('http://localhost:8080/Team/getNumberOfPeople', {
+          params: {
+            profession: 'Engineers'
+          }
+        }).toPromise()).total
+      }
+    ];
+  }
 
-    getEvents(): any {
-        return this.events;
+  async getTopCompleted(timeFrame: number) {
+    let min_date: Date;
+    switch (timeFrame) {
+      case 0:
+        min_date = new Date();
+        min_date.setDate(min_date.getDate() - 1);
+        return (await this.http.get<{ res: string }>('http://localhost:8080/Event/getTopEvent', {
+          params: {
+            date: min_date.toISOString()
+          }
+        }).toPromise()).res
+        break;
+      case 1:
+        min_date = new Date();
+        min_date.setDate(min_date.getDate() - 7);
+        return (await this.http.get<{ res: string }>('http://localhost:8080/Event/getTopEvent', {
+          params: {
+            date: min_date.toISOString()
+          }
+        }).toPromise()).res
+        break;
+      case 2:
+        min_date = new Date();
+        min_date.setDate(min_date.getDate() - 28);
+        return (await this.http.get<{ res: string }>('http://localhost:8080/Event/getTopEvent', {
+          params: {
+            date: min_date.toISOString()
+          }
+        }).toPromise()).res
+        break;
+      case 3:
+        min_date = new Date();
+        min_date.setDate(min_date.getDate() - 365);
+        return (await this.http.get<{ res: string }>('http://localhost:8080/Event/getTopEvent', {
+          params: {
+            date: min_date.toISOString()
+          }
+        }).toPromise()).res
+        break;
     }
-    
-    addEvent(event) {
-        (this.events.dataSource as Object[]).push(event);
-        this.eventEmitter.emit();
-    }
+  }
 
-    async getMostGrouped(timeFrame: number) {
-        let min_date: Date;
-        switch(timeFrame) {
-          case 0:
-            min_date = new Date();
-            min_date.setDate(min_date.getDate() - 1);
-            return [
-              {
-                name: 'Developers',
-                value: 100
-              },
-              {
-                name: 'Lawyers',
-                value: 50
-              },
-              {
-                name: 'Designers',
-                value: 80
-              },
-              {
-                name: 'Engineers',
-                value: 120
-              }
-            ];
-            break;
-          case 1:
-            min_date = new Date();
-            min_date.setDate(min_date.getDate() - 7);
-            return [
-              {
-                name: 'Developers',
-                value: 100
-              },
-              {
-                name: 'Lawyers',
-                value: 50
-              },
-              {
-                name: 'Designers',
-                value: 80
-              },
-              {
-                name: 'Engineers',
-                value: 120
-              }
-            ];
-            break;
-          case 2:
-            min_date = new Date();
-            min_date.setDate(min_date.getDate() - 28);
-            return [
-                {
-                  name: 'Developers',
-                  value: 100
-                },
-                {
-                  name: 'Lawyers',
-                  value: 50
-                },
-                {
-                  name: 'Designers',
-                  value: 80
-                },
-                {
-                  name: 'Engineers',
-                  value: 120
-                }
-              ];
-            break;
-          case 3:
-            min_date = new Date();
-            min_date.setDate(min_date.getDate() - 365);
-            return [
-                {
-                  name: 'Developers',
-                  value: 100
-                },
-                {
-                  name: 'Lawyers',
-                  value: 50
-                },
-                {
-                  name: 'Designers',
-                  value: 80
-                },
-                {
-                  name: 'Engineers',
-                  value: 120
-                }
-              ];
-            break;
-        }
-    }
+  async getPeopleInGroup(date: Date) {
+    return (await this.http.get<{ total: number }>('http://localhost:8080/Team/getAllNumberOfPeople').toPromise()).total
+  }
 
-    async getTopCompleted(timeFrame: number) {
-        let min_date: Date;
-        switch(timeFrame) {
-          case 0:
-            min_date = new Date();
-            min_date.setDate(min_date.getDate() - 1);
-            return 'Water leech';
-            break;
-          case 1:
-            min_date = new Date();
-            min_date.setDate(min_date.getDate() - 7);
-            return 'Water leech';
-            break;
-          case 2:
-            min_date = new Date();
-            min_date.setDate(min_date.getDate() - 28);
-            return 'Water leech';
-            break;
-          case 3:
-            min_date = new Date();
-            min_date.setDate(min_date.getDate() - 365);
-            return 'Water leech';
-            break;
-        }
-    }
-
-    async getPeopleInGroup(date: Date) {
-      return 231;
-    }
-    
-    async getCompletedEvents(date: Date) {
-      return 57;
-    }
+  async getCompletedEvents(date: Date) {
+    date.setDate(date.getDate() - 28);
+    return (await this.http.get<{ total: number }>('http://localhost:8080/Team/getNumberofCompletedEvent', {
+      params: {
+        date: date.toISOString()
+      }
+    }).toPromise()).total
+  }
 }
